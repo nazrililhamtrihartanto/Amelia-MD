6f7bed641650ae1576cfffa96b60c13aee28222f
nazrililhamtrihartanto <115785975+nazrililhamtrihartanto@users.noreply.github.com>
2023-02-13 11:17:37 +0700

    Update and rename plugins/welcometes.js to plugin/welcometes.js

---
+++ plugin/welcometes.js
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch'
+
+biarkan handler = async (m, { samb }) => {
+
+biarkan imgr = hwaifu.getRandom()
+
+misalkan pp = global.welcom
+
+biar nihpp = await conn.resize(imgr, 250, 280)
+
+let cap = `Selamat Datang ${conn.getName(m.sender)}\nMoga Bentah Ya`
+
+conn.sendButton(m.chat, cap, wm, Buffer.alloc(0), [['TES', 'TES'], ['TES', 'TES']], fkontak, { mimetype: "text/rtf ", fileName: ucapan, pageCount: 90000, fileLength: 90000, detik: 90000, jpegThumbnail: nihpp, contextInfo: {
+
+          externalAdReply :{
+
+          showAdAttribution: benar,
+
+    mediaUrl: "https://facebook.com",
+
+    Jenismedia: 2,
+
+    deskripsi: "WATERMAR",
+
+    judul: "Ketuk Gabung Grup Resmi",
+
+    tubuh: "ya",
+
+    thumbnail: menunggu (menunggu pengambilan(pp)).buffer(),
+
+    url sumber: sgc
+
+     }}
+
+  })
+
+}
+
+handler.command = ['wel']
+
+penangan standar ekspor
